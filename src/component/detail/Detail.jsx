ef7fdc3597fd207571f376954ddbdce9b6d0fd12
Durvesh Wagh <durveshwagh11@gmail.com>
2024-06-11 22:39:38 +0530

    intial  commit

---
+++ src/component/detail/Detail.jsx
@@ -0,0 +1,11 @@
+import "./detail.css"
+
+const Detail = () => {
+    return (
+        <div className="detail">
+            <h1>Detail</h1>
+        </div>
+    )
+}
+
+export default Detail
---
+++ src/component/list/List.jsx
@@ -0,0 +1,11 @@
+import "./list.css"
+
+const List = () => {
+    return (
+        <div className="list">
+            <h1>Lista</h1>
+        </div>
+    )
+}
+
+export default List;
